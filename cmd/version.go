32e11f1503cc83909fa4949d39e785231a6c547c
ks6088ts <ks6088ts@gmail.com>
2023-06-22 12:56:01 +0900

    implement version command

--- cmd/version.go
+++ cmd/version.go
@@ -24,21 +24,17 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/ks6088ts-labs/cogsearchctl/internal"
 	"github.com/spf13/cobra"
 )
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Print version info",
+	Long:  `Print version info`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		fmt.Printf("cogsearchctl: Version=%v, Revision=%v", internal.Version, internal.Revision)
 	},
 }
 
